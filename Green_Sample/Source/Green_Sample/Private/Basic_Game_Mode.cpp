da170849255c3559ab9714ed108fcaa023ea05f2
b1uto <112058075+b1uto@users.noreply.github.com>
2025-09-14 23:34:59 +0100

    #1 Misc

--- Green_Sample/Source/Green_Sample/Private/Basic_Game_Mode.cpp
+++ Green_Sample/Source/Green_Sample/Private/Basic_Game_Mode.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+ // Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Basic_Game_Mode.h"
